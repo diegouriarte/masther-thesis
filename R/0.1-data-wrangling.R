874fb6b0fc671139917018b941051a2e4a341b96
Diego Uriarte <diegouriarte@gmail.com>
2019-02-14 12:46:52 -0500

    change to clean file, correct prices and drop products that I am not interested in

--- R/0.1-data-wrangling.R
+++ R/0.1-data-wrangling.R
@@ -22,89 +22,117 @@ library(purrr)
 #'We load the previouly generated files that contain all information from 2005 to
 #'2008
 #'
-data_2005_2017 <- readRDS(file = here::here("data","processed","data_2005_2017.rds"))
-data_2017_2018 <- readRDS(file = here::here("data","processed","data_2017_2018.rds"))
+data_2005_2018 <- readRDS(file = here::here("data","processed","data_2005_2018.rds"))
 
 #' clean names using janitor package
-data_2005_2017 <- data_2005_2017 %>% clean_names()
-skim(data_2005_2017)
+data_2005_2018 <- data_2005_2018 %>% clean_names()
+skim(data_2005_2018)
 
 
-#' We found to missing values por año, let's see which they are:
-data_2005_2017 %>% filter(is.na(ano))
+#' We found 5 missing values in `fecha_hora`, let's see which they are:
+data_2005_2018 %>% filter(is.na(fecha_hora))
 
 #' We can drop those observation:
 #' 
-data_2005_2017 <- data_2005_2017 %>% drop_na(ano)
+data_2005_2018 <- data_2005_2018 %>% drop_na(fecha_hora)
 
-skim(data_2005_2017)
 
-#' Now, ruc still has two missing values:
+#' Now, ruc still has  missing values:
 #' 
-data_2005_2017 %>% filter(is.na(ruc)) 
+data_2005_2018 %>% filter(is.na(ruc)) 
 #' We see that is a fuel station without ruc than only appears twice in the dataset.
-#' We will drop these fuel station:
+#' Another one appears one time. I keep this observations.
+
+
+skim(data_2005_2018)
+
+#' We only keep informatio about liquid fuels
 #' 
-data_2005_2017 <- data_2005_2017 %>% drop_na(ruc)
+data_2005_2018 %>%
+    count(producto, sort = TRUE) %>% View()
 
-skim(data_2005_2017)
+productos <- data_2005_2018 %>%
+    count(producto, sort = TRUE) %>%
+    pull(producto)
 
-#' Now, unidades is completely missing, so I drop it:
-#' 
-data_2005_2017 <- data_2005_2017 %>% 
-    select(-unidades, departamento = 'nomdepa',
-           provincia = 'nomprov', distrito = 'nomdist',
-           -ano, -mes, -dia)
+level_key <- c("GASOHOL 90 PLUS" = "GASOHOL 90", 
+               "GASOHOL 84 PLUS" = "GASOHOL 84",
+               "GASOHOL 95 PLUS" = "GASOHOL 95",
+               "GASOLINA 84"  = "GASOHOL 84",
+               "GASOHOL 97 PLUS" = "GASOHOL 97",
+               "GASOLINA 90"  = "GASOHOL 90",
+               "GASOHOL 98 PLUS"  = "GASOHOL 98",
+               "Diesel B5 S-50" = "DIESEL B5 S-50 UV",
+               "DIESEL 2" = "DIESEL B2",
+               "GASOLINA 95" = "GASOHOL 95",
+               "GASOLINA 97" = "GASOHOL 97",
+               "DIESEL B5" = "DIESEL B5 UV",
+               "Diesel B5 S-50 UV" = "DIESEL B5 S-50 UV",
+               "GASOLINA 98 BA" = "GASOHOL 98",
+               "Diesel B2 S-50" = "DIESEL B5 S-50 UV",
+               "DIESEL2 BA" = "DIESEL B2",
+               "Diesel B2 S-50 UV" = "DIESEL B5 S-50 UV",
+               "DIESEL B2 BA" = "DIESEL B2",
+               "Diesel 2 S-50 UV" = "DIESEL B5 S-50 UV",
+               "DIESEL 2 UV" = "DIESEL B2",
+               "DIESEL B2 UV" = "DIESEL B2",
+               "DIESEL B5 S-50" = "DIESEL B5 S-50 UV",
+               "GASOLINA 98" = "GASOHOL 98")
 
-data_2005_2017 <- data_2005_2017 %>%
-    unite(fecha_hora, fecha_registro, hora_registro, sep = " ") %>%
-    mutate(fecha_hora = dmy_hms(fecha_hora))
 
-data_2005_2017 %>% 
-    filter(precio_venta < 10000, precio_venta > 1000) %>%
-    view()
+data_2005_2018 %>%
+    mutate(producto = recode(producto, !!!level_key)) %>%
+    count(producto, sort = TRUE) %>% 
+    View()
 
+
+data_2005_2018_clean_products <- data_2005_2018 %>%
+    mutate(producto = recode(producto, !!!level_key)) %>%
+    filter(producto != "DIESEL MARINO N° 2",
+           producto != "GLP - G",
+           producto != "KEROSENE (DOMÉSTICO)",
+           producto != "GAS NATURAL VEHICULAR",
+           producto != "GAS LICUADO DE PETROLEO")
+
+
+data_2005_2018_clean_products %>% 
+    filter(precio_de_venta < 10000, precio_de_venta > 1000) %>%
+    select(razon_social, departamento, precio_de_venta)
+
+#' Function to correct prices that have been incorrectly imputed
 correct_price <- function(price) {
-    if (price < 25) {
+    if (price < 30.1) {
         return (price)
     } else {
         return(correct_price(price/10))
     }
 }
 
-data_2005_2017 %>% 
-    mutate(precio_rev = map_dbl(data_2005_2017$precio_venta, 
+#' Test
+data_2005_2018_clean_products %>% 
+    mutate(precio_rev = map_dbl(data_2005_2018_clean_products$precio_de_venta, 
                                 correct_price),
-           diff = precio_rev == precio_venta) %>%
-    select(-2:-8) %>%
+           diff = precio_rev == precio_de_venta,
+           year = year(fecha_hora)) %>%
+    select(-2:-8,year) %>%
     filter(diff == FALSE ) %>%
-    arrange(precio_venta) %>% View()
+    arrange(precio_de_venta) %>% View()
 
-data_2005_2017 %>% 
-    mutate(precio_rev = map_dbl(data_2005_2017$precio_venta, 
+
+data_2005_2018_clean_products %>% 
+    mutate(precio_rev = map_dbl(data_2005_2018_clean_products$precio_de_venta, 
                                 correct_price),
-           diff = precio_rev == precio_venta) %>%
+           diff = precio_rev == precio_de_venta) %>%
     skim(precio_rev)
+
 
 #' Everything looks normal now with the histogram, so we save this into the object
 #' 
 #' 
-data_2005_2017 <- data_2005_2017 %>% 
-    mutate(precio_venta = map_dbl(data_2005_2017$precio_venta, 
+data_2005_2018_corrected_prices <- data_2005_2018_clean_products %>% 
+    mutate(precio_venta = map_dbl(data_2005_2018_clean_products$precio_de_venta, 
                                 correct_price))
 
-skim(data_2005_2017)
+skim(data_2005_2018_corrected_prices)
 
 
-data_2005_2017 %>% count(descripcion_producto)
-
-data_2005_2017 %>%
-    filter(departamento == "LIMA", provincia == "LIMA", distrito == "BREÑA") %>%
-    mutate(ano = year(fecha_hora)) %>%
-    select(codigo_osinerg, empresa, direccion, ano) %>%
-    distinct()
-
-data_2005_2017 %>%
-    filter(departamento == "LIMA", provincia == "LIMA") %>%
-    count(distrito, direccion) %>%
-    count(distrito) %>% View()
