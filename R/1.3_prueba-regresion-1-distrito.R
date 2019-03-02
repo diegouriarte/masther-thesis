66127c7d6919d7a02536269733029f126d650505
Diego Uriarte <diegouriarte@gmail.com>
2019-03-02 11:14:37 -0500

    Âse consigue una función para precios semanales x diesel. falta para gasolina y limpiar archivo

--- R/1.3_prueba-regresion-1-distrito.R
+++ R/1.3_prueba-regresion-1-distrito.R
@@ -69,7 +69,7 @@ grifo_17944_fun %>% View
 #' 
 #' 
 
-for (year in c(2006:2018)) {
+for (year in c(min(grifo_17944$año):2018)) {
   for (week in c(1:52)) {
       if (week %ni% grifo_17944[grifo_17944$año == year,]$semana) {
           if (week == 1) {
@@ -101,7 +101,7 @@ grifo_17944 %>% arrange(codigo_de_osinergmin, año, semana) %>% View
 #' Creamos una función para hacerlo por grifo:
 #' 
 PreciosSemanaGrifo <- function(df_grifo) {
-    for (year in c(2006:2018)) {
+    for (year in c(min(df_grifo$año):2018)) {
         for (week in c(1:52)) {
             if (week %ni% df_grifo[df_grifo$año == year,]$semana) {
                 if (week == 1) {
@@ -131,36 +131,32 @@ PreciosSemanaGrifo <- function(df_grifo) {
 }
 
 
-for (prod in unique(grifo_17944_fun$producto)) {
-    grifo_17944_fun %>% filter(producto == prod) %>%
-        PreciosSemanaGrifo(.)
-}
+
 grifo_17944_fun <- PreciosSemanaGrifo(grifo_17944_fun)
 grifo_17944_fun %>% View()
 
 
-#' Ahora hacemos lo mismo pero para todos los grifos de la muestra:
-#' 
-
-name <- function(df) {
-    df %>%
-        select(-ruc:-provincia,-direccion, -unidad) %>%
-        mutate(semana = week(fecha_hora), `aÃ±o` = year(fecha_hora),
-               semana_inicio = semana + (`aÃ±o` - 2010)*52) %>%
-        arrange(codigo_de_osinergmin, fecha_hora, producto) %>%
-        filter(codigo_de_osinergmin== "17944") %>%
-        distinct() %>%
-        group_by_at(.vars = vars(-precio_de_venta,-fecha_hora)) %>%
-        summarize(precio_de_venta = mean(precio_de_venta)) %>%
-        ungroup()
-}
 
 #' Sacamos un precio por semana para cada grifo en el caso del diesel
 
 lista_grifos <- prices_lima %>% distinct(codigo_de_osinergmin) %>% pull()
-df_list <- list()
+list_diesel <- list()
 for (grifo in lista_grifos) {
-    df_list[[grifo]] <- prices_lima %>% 
+    list_diesel[[grifo]] <- prices_lima %>% 
+        filter(producto == "DIESEL", codigo_de_osinergmin == grifo) %>%
+        PrecioPromedioPorSemana(.)
+    
+}
+
+
+
+
+#' Sacamos un precio por semana para cada grifo en el caso del diesel
+
+lista_grifos <- prices_lima %>% distinct(codigo_de_osinergmin) %>% pull()
+list_diesel <- list()
+for (grifo in lista_grifos) {
+    list_diesel[[grifo]] <- prices_lima %>% 
         filter(producto == "DIESEL", codigo_de_osinergmin == grifo) %>%
         PrecioPromedioPorSemana(.)
     
@@ -168,21 +164,40 @@ for (grifo in lista_grifos) {
 
 #' 
 
-bind_rows(df_list) %>% View()
+df_diesel <- bind_rows(list_diesel)
 
-
+df_diesel
 
 #####################
 
 
 grifo_100155 <- prices_lima %>%
-    filter(codigo_de_osinergmin == "100155") %>%
+    filter(codigo_de_osinergmin == "9584", producto == "DIESEL") %>%
     PrecioPromedioPorSemana(.)
 
 grifo_100155_semana <- PreciosSemanaGrifo(grifo_100155)
+grifo_100155_semana <- PreciosSemanaGrifo2(grifo_100155)
 
+grifo_100155_semana %>% View()
+#' Todo OK con la prueba.
+#' 
+#' Repetimos para Diesel, y todos los grifos
 
-grifo_100155_semana
+list_diesel_semana <- list()
+i <- 1
+for (grifo in lista_grifos) {
+    list_diesel_semana[[grifo]] <- df_diesel %>% 
+        filter(codigo_de_osinergmin == grifo) %>%
+        PreciosSemanaGrifo(.) 
+    i <- i+1
+    print(c(grifo, i))
+}
+
+#' 
+
+bind_rows(df_list) 
+
+grifo_100155_semana %>% View
 prices_lima %>%
     filter(distrito == "BREÑA") %>%
     select(-ruc:-provincia,-direccion, -unidad) %>%
@@ -193,4 +208,138 @@ prices_lima %>%
     summarize(precio_de_venta = mean(precio_de_venta)) %>%
     arrange(codigo_de_osinergmin, producto, año, semana) %>%
     ungroup()
+
+prices_lima %>%
+    filter(codigo_de_osinergmin == "100012", producto == "DIESEL") %>%
+    View()
+
+
+###############otra prueba############
+
+grifo <- prices_lima %>%
+    filter(codigo_de_osinergmin == "9584", producto == "DIESEL") %>%
+    PrecioPromedioPorSemana(.)
+
+grifo %>% View
+#' Otra prueba para tener precios x semana
+#' 
+#' 
+
+for (year in c(min(grifo$año):2018)) {
+    if (year == min(grifo$año)) {
+        for (week in (min(grifo[grifo$año == year,]$semana) + 1):53) {
+            if (week %ni% grifo[grifo$año == year, ]$semana) {
+                grifo <- bind_rows(
+                    grifo,
+                    filter(grifo,
+                           `año` == year,
+                           semana == week - 1) %>%
+                        mutate(semana = week)
+                )
+            }
+        }
+    } else {
+    
+    for (week in c(1:53)) {
+        if (week %ni% grifo[grifo$año == year,]$semana) {
+            if (week == 1) {
+                grifo <- bind_rows(
+                    grifo,
+                    filter(grifo,
+                           `año` == year - 1,
+                           semana == max(grifo[grifo$año == year -
+                                                         1, ]$semana)) %>%
+                        mutate(semana = week,
+                               `año` = year)
+                )
+            } else {
+                grifo <- bind_rows(grifo,
+                                         filter(grifo, 
+                                                `año` == year, 
+                                                semana == week - 1) %>%
+                                             mutate(semana = week))
+                
+            }
+        } 
+    }
+}
+} 
+
+
+grifo %>% arrange(codigo_de_osinergmin, año, semana) %>% View
+
+## Este código funciona! Escribamos una función y probemos
+
+
+PreciosSemanaGrifo2 <- function(grifo) {
+    for (year in c(min(grifo$año):2018)) {
+        if (year == min(grifo$año)) {
+            for (week in (min(grifo[grifo$año == year, ]$semana) + 1):53) {
+                if (week %ni% grifo[grifo$año == year,]$semana) {
+                    grifo <- bind_rows(
+                        grifo,
+                        filter(grifo,
+                               `año` == year,
+                               semana == week - 1) %>%
+                            mutate(semana = week)
+                    )
+                }
+            }
+        } else {
+            for (week in c(1:53)) {
+                if (week %ni% grifo[grifo$año == year, ]$semana) {
+                    if (week == 1) {
+                        grifo <- bind_rows(
+                            grifo,
+                            filter(
+                                grifo,
+                                `año` == year - 1,
+                                semana == max(grifo[grifo$año == year -
+                                                        1,]$semana)
+                            ) %>%
+                                mutate(semana = week,
+                                       `año` = year)
+                        )
+                    } else {
+                        grifo <- bind_rows(
+                            grifo,
+                            filter(grifo,
+                                   `año` == year,
+                                   semana == week - 1) %>%
+                                mutate(semana = week)
+                        )
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    grifo %>% arrange(codigo_de_osinergmin, año, semana)
+}
+
+grifo <- prices_lima %>%
+    filter(codigo_de_osinergmin == "9592", producto == "DIESEL") %>%
+    PrecioPromedioPorSemana(.)
+grifo %>% View()
+
+grifo <- PreciosSemanaGrifo2(grifo)
+
+## Todo ok, ahora hagamos para todo lima!
+
+list_diesel_semana <- list()
+i <- 1
+for (grifo in lista_grifos) {
+    if (nrow(df_diesel[df_diesel$codigo_de_osinergmin == grifo,])>0) {
+        list_diesel_semana[[grifo]] <- df_diesel %>% 
+            filter(codigo_de_osinergmin == grifo) %>%
+            PreciosSemanaGrifo2(.) 
+        i <- i+1
+        print(c(grifo, i)) 
+    }
+
+}
+
+df_diesel_semana <- bind_rows(list_diesel_semana)
 
