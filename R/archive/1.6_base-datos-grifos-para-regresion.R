cf978609f5f62bb511f5aaca8036dd87bf7bdedf
Diego Uriarte <diegouriarte@gmail.com>
2019-03-31 10:35:57 -0500

    1.5 -> se retira un grifo más por no tener datos 1.6 -> se limpia base de datos corregiendo errores de codificación de grifos

--- R/archive/1.6_base-datos-grifos-para-regresion.R
+++ R/archive/1.6_base-datos-grifos-para-regresion.R
@@ -5,13 +5,15 @@
 #' output: github_document
 #' ---
 #' 
+
+#' Cargamos las librerÃ­as
 suppressMessages(library(dplyr))
 suppressMessages(library(tidyverse))
 library(lubridate)
 library(stringr)
 source(file = "R/1.5.1_funcion-auxiliar-distancia.R")
 
-distancia_competencia <- 3
+distancia_competencia <- 1.5
 
 grifos_datos_coding <- readRDS(here::here("data","processed","grifo_coding_raw_no_duplicate.rds")) %>%
     mutate(lat = as.double(lat))
@@ -29,9 +31,9 @@ grifos_datos_coding_nombres <- grifos_datos_coding %>%
                                    "PRIMAX" = "PRIMAX",
                                    "REPSOL" = "REPSOL",
                                    "PETROPERU" = "PETROPERU",
-                                   "PETROPERÚ" = "PETROPERU",
+                                   "PETROPER?" = "PETROPERU",
                                    "REPSOL-GAZEL" = "REPSOL",
-                                   "PetroPerú" = "PETROPERU",
+                                   "PetroPer?" = "PETROPERU",
                                    .default = "INDEPENDIENTE"))
 
 grifos_datos_coding_nombres %>%   count(bandera)
@@ -42,17 +44,17 @@ grifos_datos_coding_nombres %>% count(razon_social, bandera, sort = TRUE) %>%
 grifos_datos_coding_nombres %>% count(razon_social, bandera, sort = TRUE) %>%
     filter(str_detect(razon_social, "PERUANA"))
 #' Agregamos la variable si el grifo es propio, abanderado o independiente
-#' además, una variable dicotómica por si ofrece GNV o GLP
+#' adem?s, una variable dicot?mica por si ofrece GNV o GLP
 #' 
 grifo_datos_con_tipo <- grifos_datos_coding_nombres %>% 
     mutate(
         tipo = case_when(
-        str_detect(razon_social, "REPSOL") ~ "PROPIA",
-        str_detect(razon_social, "COESTI") ~ "PROPIA",
-        str_detect(razon_social, "PERUANA DE ESTACIONES") ~ "PROPIA",
-        bandera == "PETROPERU" ~ "ABANDERADA",
-        bandera == "INDEPENDIENTE" ~ "INDEPENDIENTE",
-        TRUE ~ "ABANDERADA"
+            str_detect(razon_social, "REPSOL") ~ "PROPIA",
+            str_detect(razon_social, "COESTI") ~ "PROPIA",
+            str_detect(razon_social, "PERUANA DE ESTACIONES") ~ "PROPIA",
+            bandera == "PETROPERU" ~ "ABANDERADA",
+            bandera == "INDEPENDIENTE" ~ "INDEPENDIENTE",
+            TRUE ~ "ABANDERADA"
         ),
         con_glp = if_else(!is.na(glp), 1, 0),
         con_gnv = if_else(!is.na(gnv), 1, 0),
@@ -66,7 +68,7 @@ filter(grifo_datos_con_tipo, bandera == "INDEPENDIENTE", tipo == "PROPIA")
 glimpse(grifo_datos_con_tipo)
 
 
-#' Ahora calculamos los distancia al grifo más cercano
+#' Ahora calculamos los distancia al grifo m?s cercano
 #' 
 #' Filtramos solo aquellos filtros con coordenadas
 
@@ -75,7 +77,7 @@ grifos_con_datos <- grifo_datos_con_tipo %>%
 
 grifos_con_datos
 
-#' Hallamos la distancia al grifo más cercano
+#' Hallamos la distancia al grifo m?s cercano
 #'
 matriz_distancias <- grifos_con_datos %>%
     select("name" = codigo_de_osinergmin , lat, lon) %>%
@@ -85,7 +87,7 @@ matriz_distancias <- grifos_con_datos %>%
 matriz_distancias[1,]
 
 #' Generamos un archivo con las distancias para revisar manualmente errores de
-#' codificación
+#' codificaci?n
 as_tibble(matriz_distancias, rownames = "codigo_de_osinergmin") %>%
     gather(key = "grifo_distancia", value = "distancia", -codigo_de_osinergmin) %>%
     filter(distancia != 0) %>%
@@ -97,8 +99,8 @@ as_tibble(matriz_distancias, rownames = "codigo_de_osinergmin") %>%
     arrange(distancia_min) %>%
     write_excel_csv(path = here::here("data", "processed","grifos-distancia.csv"))
 
-#' Creamos variable con distancia mínima al grifo competidor más cercano
-#' Pendiente, como hacer para que no considere si el grifo más cercano tiene el mismo ruc
+#' Creamos variable con distancia m?nima al grifo competidor m?s cercano
+#' Pendiente, como hacer para que no considere si el grifo m?s cercano tiene el mismo ruc
 grifos_distancia_minima <- as_tibble(matriz_distancias, rownames = "codigo_de_osinergmin") %>%
     gather(key = "grifo_distancia", value = "distancia", -codigo_de_osinergmin) %>%
     filter(distancia != 0) %>%
@@ -125,7 +127,7 @@ grifos_distancia_promedio <- as_tibble(matriz_distancias, rownames = "codigo_de_
 
 #' Lo agregamos al archivo anterior
 grifos_distancias <- full_join(grifos_distancia_minima, grifos_distancia_promedio,
-          by = "codigo_de_osinergmin") %>%
+                               by = "codigo_de_osinergmin") %>%
     rename("grifo_mas_cercano" = grifo_distancia) %>%
     mutate(codigo_de_osinergmin = as.numeric(codigo_de_osinergmin))
 
