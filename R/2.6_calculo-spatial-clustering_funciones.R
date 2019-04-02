1fab4ce48e3e9c915c366caab79604aa43405bb5
Diego Uriarte <diegouriarte@gmail.com>
2019-04-02 17:39:04 -0500

    modificacmos funciones de sc para que admitan argumento de bandera o razon social

--- R/2.6_calculo-spatial-clustering_funciones.R
+++ R/2.6_calculo-spatial-clustering_funciones.R
@@ -26,9 +26,9 @@ hallar_lista_vecinos <- function(cod1) {
     grifos[grifos$codigo_de_osinergmin.princ == cod1, ]$codigo_de_osinergmin.vec
 }
 
-hallar_cluster <- function(cod) {
+hallar_cluster <- function(cod, tipo = "razon_social") {
     #halla todas las estaciones vecinas que tienen la misma bandera
-    #input: codigo de la estación
+    #input: codigo de la estación, tipo de cluster (razon_social o bandera)
     #output: dataframe que tiene todas las estacioens del cluster de cod,
     #        incluida cod, con razón social y bandera
     
@@ -36,42 +36,60 @@ hallar_cluster <- function(cod) {
         distinct(codigo_de_osinergmin.princ,
                  razon_social.princ,
                  bandera.princ)
-    bandera <- hallar_bandera(cod)
     lista_vecinos <- hallar_lista_vecinos(cod)
-    df2 <- filter(grifos,
-                  codigo_de_osinergmin.princ %in% lista_vecinos,
-                  bandera.princ == bandera) %>%
-        distinct(codigo_de_osinergmin.princ,
-                 razon_social.princ,
-                 bandera.princ)
+    
+    bandera <- hallar_bandera(cod)
+    if (tipo == "razon_social") {
+        bandera <-
+            unique(grifos[grifos$codigo_de_osinergmin.princ == cod,]$bandera.princ)
+        df2 <- filter(grifos,
+                      codigo_de_osinergmin.princ %in% lista_vecinos,
+                      bandera.princ == bandera) %>%
+            distinct(codigo_de_osinergmin.princ,
+                     razon_social.princ,
+                     bandera.princ)
+    } else {
+        razon_social_cod <-
+            unique(grifos[grifos$codigo_de_osinergmin.princ == cod,]$razon_social.princ)
+        df2 <- filter(grifos,
+                      codigo_de_osinergmin.princ %in% lista_vecinos,
+                      razon_social.princ == razon_social_cod) %>%
+            distinct(codigo_de_osinergmin.princ,
+                     razon_social.princ,
+                     bandera.princ)
+    }
+    
     bind_rows(df1, df2)
 }
 
-hallar_todos_cluster <- function(cod) {
+hallar_todos_cluster <- function(cod, tipo = "razon_social") {
     #halla todas las que están en mismo cluster (a nivel de banderas) que cod,
     #sin importar el nivel
     #input: codigo de la estación
     #output: dataframe que tiene todas las estaciones del cluster de cod,
     #        incluida cod, con razón social y bandera
-    if (hallar_bandera(cod) == "INDEPENDIENTE") {
+    
+    if (tipo == "bandera" & hallar_bandera(cod) == "INDEPENDIENTE") {
         df1 <- filter(grifos, codigo_de_osinergmin.princ == cod) %>%
             distinct(codigo_de_osinergmin.princ,
                      razon_social.princ,
                      bandera.princ)
         return(df1)
     }
+    
     i <- 1
-    df <- hallar_cluster(cod)
+    df <- hallar_cluster(cod, tipo)
     
     while (i < nrow(df)) {
         df <-
-            bind_rows(df, hallar_cluster(df$codigo_de_osinergmin.princ[i + 1])) %>%
+            bind_rows(df, hallar_cluster(df$codigo_de_osinergmin.princ[i + 1]), tipo) %>%
             distinct()
         i <- i + 1
     }
     
     arrange(df, codigo_de_osinergmin.princ)
 }
+
 
 comprobar_df_dup <- function(df1, df2) {
     #Funcionar para comprobar si dos dataframes son iguales. Es para elimitar los 
@@ -92,7 +110,7 @@ comprobar_df_dup <- function(df1, df2) {
     }
 }
 
-hallar_clusteres_mercado <- function(cod) {
+hallar_clusteres_mercado <- function(cod, tipo = "razon_social") {
     #halla todos clusteres del mercado que genera el grifo cod
     #input: codigo de la estación
     #output: lista que contiene dataframes con cada cluster que rodea a la 
@@ -107,12 +125,13 @@ hallar_clusteres_mercado <- function(cod) {
                bandera = bandera.vec)
     
     grifo_con_vecinos <- bind_rows(df1, df2)
+    
     #creamos lista con cluster de cada grifo (pueden haber repetidos)
     
     lista <- list()
     
     for (grifo in grifo_con_vecinos$codigo) {
-        lista[[as.character(grifo)]] <- hallar_todos_cluster(grifo)
+        lista[[as.character(grifo)]] <- hallar_todos_cluster(grifo, tipo)
     }
     
     #removemos los df repetidos (que normalmente habrá si hay vecinos en el mercado
@@ -130,11 +149,11 @@ hallar_clusteres_mercado <- function(cod) {
 }
 
 
-calcular_sc <- function(cod) {
+calcular_sc <- function(cod, tipo = "razon_social") {
     #numero de grifos en el mercado local
     N <- length(hallar_lista_vecinos(cod)) + 1
     #número de clusteres
-    (M <- length(hallar_clusteres_mercado(cod)))
+    M <- length(hallar_clusteres_mercado(cod))
     #suma número de estaciones en cada cluster
     suma_clusteres <- sum(sapply(hallar_clusteres_mercado(cod), nrow))
     suma_clusteres
