8c4f89642cbab39742816b231c53179aee264dfd
Diego Uriarte <diegouriarte@gmail.com>
2021-03-11 12:46:11 -0500

    correcciones de formato. En level_key se agregan variables adicionales de Diesel

--- R/01_import-files-from-excel.R
+++ R/01_import-files-from-excel.R
@@ -16,44 +16,53 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 library(lubridate)
+library(here)
 
-#' Hay dos hojas que requieren ser importadas del primer archivo, las demás 
+#' Hay dos hojas que requieren ser importadas del primer archivo, las demas 
 #' contienen data de GN o GLP
 
-data_2005_2017_sheet1 <- read_excel(here::here("data","no-sync","1_fuel-station-price-data",
-                                               "base-de-datos-precios-combustibles-2005-2017.xlsx"), 
-                                    sheet ="EVP_01", col_names = TRUE,
-                                    skip = 3,progress = readxl_progress()) 
+data_2005_2017_sheet1 <- read_excel(here("data","no-sync",
+                                         "1_fuel-station-price-data",
+                                         "base-de-datos-precios-combustibles-2005-2017.xlsx"), 
+                                    sheet ="EVP_01", 
+                                    col_names = TRUE,
+                                    skip = 3,
+                                    progress = readxl_progress()) 
 
+data_2005_2017_sheet2 <- read_excel(here("data", "no-sync",
+                                         "1_fuel-station-price-data",
+                                         "base-de-datos-precios-combustibles-2005-2017.xlsx"),
+                                    sheet = "EVP_02",
+                                    col_names = TRUE,
+                                    skip = 3,
+                                    progress = readxl_progress()) 
 
-data_2005_2017_sheet2 <- read_excel(here::here("data","no-sync","1_fuel-station-price-data",
-                                  "base-de-datos-precios-combustibles-2005-2017.xlsx"), 
-                      sheet ="EVP_02", col_names = TRUE,
-                      skip = 3,progress = readxl_progress()) 
 
 data_2005_2017 <- bind_rows(data_2005_2017_sheet1, data_2005_2017_sheet2)
 
+#' Complementamos con la data 2017-2018 que estÃ¡ en otro archivo
+data_2017_2018 <- read_excel(here("data", "no-sync", 
+                                  "1_fuel-station-price-data",
+                                  "base-de-datos-precios-combustibles-2017-2018.xlsx"),
+                             sheet = "PRICE",
+                             col_names = TRUE,
+                             skip = 3)
 
-data_2017_2018 <- read_excel(here::here("data","no-sync","1_fuel-station-price-data",
-                                  "base-de-datos-precios-combustibles-2017-2018.xlsx"), 
-                             sheet ="PRICE", col_names = TRUE,
-                             skip = 3) 
 glimpse(data_2005_2017)
 glimpse(data_2017_2018)
 
-data_2005_2017_selected <- data_2005_2017 %>% 
-    select(-DESCRIPCION_ACTIVIDAD, -AÑO, -MES, -DIA)
+data_2005_2017_selected <- data_2005_2017 %>%
+  select(-DESCRIPCION_ACTIVIDAD, -AÃ‘O, -MES, -DIA)
+
 
 colnames(data_2005_2017_selected)
 colnames(data_2017_2018)
 
-#'set same colnames to both objects
+#'Uniformizamos los nombres de las columnas, ya que describen lo mismo
 colnames(data_2005_2017_selected) <- colnames(data_2017_2018)
 
-glimpse(data_2005_2017_selected)
-glimpse(data_2017_2018)
 
-#' Correct date format to first object
+#' Creamos una sola columna con fecha y hora, para que R lo reconozca como fecha
 data_2005_2017_selected <- data_2005_2017_selected %>%
     unite(fecha_hora, `Fecha de Registro`, `Hora de Registro`, sep = " ") %>%
     mutate(fecha_hora = dmy_hms(fecha_hora))
@@ -61,7 +70,7 @@ data_2005_2017_selected <- data_2005_2017_selected %>%
 glimpse(data_2005_2017_selected)
 head(data_2017_2018)
 
-#'correct date format to second object
+#'Lo mismo para el archivo 2017-2018
 data_2017_2018_date <- data_2017_2018 %>%
     mutate(hora = hour(`Hora de Registro`),
            minuto = minute(`Hora de Registro`),
@@ -69,12 +78,9 @@ data_2017_2018_date <- data_2017_2018 %>%
            fecha_hora = `Fecha de Registro` + hours(hora) + minutes(minuto) + seconds(segundo))  %>%
     select(-hora, -minuto,-hora, -segundo, - `Hora de Registro`, -`Fecha de Registro`)
 
-glimpse(data_2017_2018_date)
-glimpse(data_2005_2017_selected)
 
-#combine two objects to get data from 2005 to 2018
 data_2005_2018 <- bind_rows(data_2005_2017_selected, data_2017_2018_date)
 
 #' Save datafile
-saveRDS(data_2005_2018, file = here::here("data","processed","data_2005_2018.rds"))
+saveRDS(data_2005_2018, file = here("data","processed","data_2005_2018.rds"))
 
--- R/02_data-wrangling.R
+++ R/02_data-wrangling.R
@@ -5,25 +5,28 @@
 #' output: github_document
 #' ---
 #'
-#'The purpose of this R file is to clean the database from mistakes such as product
-#'names, incorrect prices. Also, I drop observations from LPG and Natural Gas
-#'
+#'El objetivo de este archivo R es tomar la base de datos obtenida en
+#'01_import-files-from-excel.R y detectar y limpiar errores como nombres de
+#'productos que no sean idénticos, precios incorrectos. También se retiran
+#'precios de gas natural y GLP que no son necesarios. Se corrige para todo el
+#'país, con la observación que hay varias observaciones en Provincia con precio 0, 
+#'que no corrijo por no ser de interés en esta oportunidad.
 #'
 
-#'Packages to use
+#'Paquetes a utilizar
 #'
 library(here)
 library(tidyverse)
-library(readxl)
 library(janitor)
-library(skimr)
 library(lubridate)
 library(purrr)
+library(skimr)
+#library(readxl)
 
 #'We load the previouly generated files that contain all information from 2005 to
 #'2008
 #'
-data_2005_2018 <- readRDS(file = here::here("data","processed","data_2005_2018.rds"))
+data_2005_2018 <- readRDS(file = here("data","processed","data_2005_2018.rds"))
 
 #' clean names using janitor package
 data_2005_2018 <- data_2005_2018 %>% clean_names()
@@ -32,7 +35,7 @@ data_2005_2018 <- data_2005_2018 %>% clean_names()
 #' We found 5 missing values in `fecha_hora`, let's see which they are:
 data_2005_2018 %>% filter(is.na(fecha_hora))
 
-#' We can drop those observation:
+#' We can drop those observations:
 #' 
 data_2005_2018 <- data_2005_2018 %>% drop_na(fecha_hora)
 
@@ -44,25 +47,27 @@ data_2005_2018 %>% filter(is.na(ruc))
 #' Another one appears one time. I keep this observations.
 
 
-#' We only keep informatio about liquid fuels
+#' We only keep information about liquid fuels
 #' 
-data_2005_2018 %>%
-    filter(departamento == "LIMA", provincia == "LIMA",
-           year(fecha_hora) < 2011) %>%
-    count(producto, sort = TRUE) 
-
-
 
 productos <- data_2005_2018 %>%
     count(producto, sort = TRUE) %>%
     pull(producto)
 
-estaciones_venden_DIESEL2BA <- data_2005_2018 %>% 
-    filter(departamento == "LIMA", provincia == "LIMA") %>%
-    filter(producto == "DIESEL2 BA") %>%
-    distinct(codigo_de_osinergmin) %>% pull()
+productos
+
+#' As the stations use different names for the fuels, we need to convert to the 
+#' same
+# 
+# estaciones_venden_DIESEL2BA <- data_2005_2018 %>%
+#   filter(departamento == "LIMA", provincia == "LIMA") %>%
+#   filter(producto == "DIESEL2 BA") %>%
+#   distinct(codigo_de_osinergmin) %>%
+#   pull()
 
     
+#' Este es el primer cambio al códidgo de hace años, agrego algunas keys
+#' más de die
 
 level_key <- c("GASOHOL 90 PLUS" = "G90", 
                "GASOHOL 84 PLUS" = "G84",
@@ -84,58 +89,60 @@ level_key <- c("GASOHOL 90 PLUS" = "G90",
                "DIESEL 2 UV" = "DIESEL",
                "DIESEL B2 UV" = "DIESEL",
                "DIESEL B5 S-50" = "DIESEL",
-               "GASOLINA 98" = "G98")
-
-
-data_2005_2018 %>%
-    mutate(producto = recode(producto, !!!level_key)) %>%
-    count(producto, sort = TRUE)
+               "GASOLINA 98" = "G98",
+               "DIESEL B2" = "DIESEL",
+               "DIESEL B5 UV" = "DIESEL",
+               "DIESEL B2" = "DIESEL",
+               "DIESEL2 BA" = "DIESEL",
+               "DIESEL B2 BA" = "DIESEL")
 
 
 data_2005_2018_clean_products <- data_2005_2018 %>%
     mutate(producto = recode(producto, !!!level_key)) %>%
-    filter(producto != "DIESEL MARINO N? 2",
+    filter(producto != "DIESEL MARINO N° 2",
            producto != "GLP - G",
-           producto != "KEROSENE (DOM?STICO)",
+           producto != "KEROSENE (DOMÉSTICO)",
            producto != "GAS NATURAL VEHICULAR",
-           producto != "GAS LICUADO DE PETROLEO",
-           producto != "DIESEL2 BA", 
-           producto != "DIESEL B2 BA")
+           producto != "GAS LICUADO DE PETROLEO")
 
 
 data_2005_2018_clean_products %>% 
-    filter(precio_de_venta < 10000, precio_de_venta > 1000) %>%
-    select(razon_social, departamento, precio_de_venta)
+    filter(precio_de_venta > 1000) %>%
+    ggplot(aes(precio_de_venta)) + 
+    geom_histogram()
 
 data_2005_2018_clean_products %>% 
-    filter(precio_de_venta == 1, 
-           departamento == "LIMA", 
-           provincia == "LIMA",
-           year(fecha_hora) > 2016,
-           producto == "DIESEL") %>%
-    select(razon_social, fecha_hora, precio_de_venta) %>% 
-    count(razon_social)
+    filter(precio_de_venta > 100000)
+#' De la gráfica y filtro, hay un precio super alto, que corresponde a una estación en
+#' Comas en 2005 que lo imputó incorrectamente. 
+#' 
+#' 
 
 
 
 #' Function to correct prices that have been incorrectly imputed
 correct_price <- function(price) {
-    if (price < 30.1) {
-        return (price)
-    } else {
-        return(correct_price(price/10))
-    }
+  if (price < 30.1) {
+    return(price)
+  } else {
+    return(correct_price(price / 10))
+  }
 }
 
-#' Test
-data_2005_2018_clean_products %>% 
-    mutate(precio_rev = map_dbl(data_2005_2018_clean_products$precio_de_venta, 
-                                correct_price),
-           diff = precio_rev == precio_de_venta,
-           year = year(fecha_hora)) %>%
-    select(-2:-8,year) %>%
-    filter(diff == FALSE ) %>%
-    arrange(precio_de_venta)
+#' Al hacer un prueba, se ve que se corrige la distribución
+data_2005_2018_clean_products %>%
+  mutate(
+    precio_rev = map_dbl(
+      data_2005_2018_clean_products$precio_de_venta,
+      correct_price
+    ),
+    diff = precio_rev == precio_de_venta,
+    year = year(fecha_hora)
+  ) %>%
+  select(-2:-8, year) %>%
+  filter(diff == FALSE) %>%
+  skim(precio_rev)
+
 
 
 data_2005_2018_clean_products %>% 
@@ -149,26 +156,27 @@ data_2005_2018_clean_products %>%
 #' 
 #' 
 data_2005_2018_corrected_prices <- data_2005_2018_clean_products %>% 
-    mutate(precio_de_venta = map_dbl(data_2005_2018_clean_products$precio_de_venta, 
-                                correct_price))
+  mutate(precio_de_venta_1 = map_dbl(data_2005_2018_clean_products$precio_de_venta, 
+                                   correct_price))
 
-skim(data_2005_2018_corrected_prices)
+skim(data_2005_2018_corrected_prices, precio_de_venta)
 
-#' No puede ser que el mínimo sea 0, corrijamos:
+#' No puede ser que el mínimo sea 0, veamos que está ocurriendo:
 #' 
 
+data_2005_2018_corrected_prices %>%
+  filter(precio_de_venta == 0) %>% 
+  count(departamento) %>% 
+  arrange(desc(n)) %>%
+  pull(departamento)
+
+#' Como el problema no está en Lima, sigamos, no tengo tiempo para corregir
 
 
 data_2005_2018_corrected_prices_1 <- data_2005_2018_corrected_prices %>% 
     filter(precio_de_venta != 0,
            precio_de_venta > 4)
 
-data_2005_2018_corrected_prices_1 %>% 
-    filter(precio_de_venta < 6) %>%
-    pull(precio_de_venta) %>% 
-    hist()
 
-
-skim(data_2005_2018_corrected_prices_1)
 saveRDS(data_2005_2018_corrected_prices_1, file = here::here("data","processed","data_2005_2018_clean.rds"))
 
