b31c434332020cc5c3fd2a471a1e805701d529f6
Diego Uriarte <diegouriarte@gmail.com>
2021-03-19 17:56:54 -0500

    se crean distintas gráficas de PECSA versus grupos de control

--- R/adicional07_estadisticas-tablas-y-graficos.R
+++ R/adicional07_estadisticas-tablas-y-graficos.R
@@ -1,5 +1,6 @@
 
 library(tidyverse)
+'%ni%' <- Negate('%in%')
 
 # Datos de porcejtan de estaciones Pecsa y Repsol propias---------------------------------------------
 
@@ -87,62 +88,6 @@ p1 <- data_total %>%
     
 p1 
 
-distritos_con_pecsa <- data_total %>% 
-    distinct(distrito, tipo_bandera) %>% 
-    filter(tipo_bandera == "PROPIA PECSA") %>% 
-    pull(distrito)
-
-
-
-f_labels <- data.frame(producto = c("DIESEL", "G90"), label = c("", "Venta de Pecsa"))
-
-
-(p2 <- data_total %>% 
-    filter(fecha <= dmy("2-10-2018"),
-           distrito %in% distritos_con_pecsa) %>% 
-    group_by(fecha, producto, tipo_bandera) %>% 
-    summarise(precio_promedio = mean(precio_de_venta)) %>% 
-    filter(tipo_bandera %in% c("PROPIA PRIMAX", "PROPIA PECSA")) %>%
-    mutate(tipo_bandera = fct_relevel(tipo_bandera, "PROPIA PRIMAX")) %>% 
-    ggplot() +
-    geom_line(aes(x = fecha, y = precio_promedio, linetype = tipo_bandera),
-              size = 1) +
-    geom_line(data = data_total %>%
-                  filter(fecha <= dmy("2-10-2018"),
-                         distrito %in% distritos_con_pecsa) %>%
-                  group_by(fecha, producto, tipo) %>%
-                  summarise(precio_promedio = mean(precio_de_venta)) ,
-              aes(x = fecha, y = precio_promedio, color = tipo),
-              size = 1.2) +
-    facet_grid(producto ~ .) +
-    geom_vline(xintercept = dmy("01-02-2018"),
-               linetype = "dashed",
-               size = 1.2, color = "grey55") +
-    geom_text(x = dmy("01-02-2018"), y = -Inf, vjust = -0.9, 
-              hjust = -0.1, aes(label = label), data = f_labels,
-              color = "grey55", fontface = "bold") +
-    labs(x = "Fecha", y = "Precio promedio", 
-         color = "Tipo de estación",
-         linetype = "Venta Pecsa") +
-    theme_bw() + 
-    scale_color_brewer(palette = "Set1") + 
-    theme(
-        panel.spacing.x = unit(0.8, "cm"),
-        legend.background = element_rect(fill = "grey85", colour = "black", size = 0.5),
-        legend.title = element_text(face = "bold", size = 10),
-        legend.text = element_text(colour = "black"),
-        legend.key = element_rect(colour = "black", size = 0.5),
-        axis.title = element_text(face = "bold"),
-        legend.position = "right"
-        
-    )
-)
-
-ggsave("precios-tipo-grifo.png", 
-       path = here::here("plots"), device = "png", unit = "cm",
-       height = 15,
-       width = 15,
-       dpi = 300)
     
 data_db5 %>% 
     mutate(fecha = lubridate::dmy(fecha)) %>% 
---
+++ R/plot04_comparativa_PECSA_control.R
@@ -0,0 +1,176 @@
+#' Saca distintos tipos de gráficos comparando PECSA con el resto de estaciones
+#' 
+#' 
+#' Se prueban con distintos grupos de control
+#' 
+#' 
+# Cargamos librerías y definición de tema ---------------------------
+library(tidyverse)
+library(lubridate)
+'%ni%' <- Negate('%in%')
+tema_grafica <- theme_bw() + 
+  theme(
+    panel.spacing.x = unit(0.8, "cm"),
+    legend.background = element_rect(fill = "grey85", colour = "black", size = 0.5),
+    legend.title = element_text(face = "bold", size = 10),
+    legend.text = element_text(colour = "black"),
+    legend.key = element_rect(colour = "black", size = 0.5),
+    axis.title = element_text(face = "bold"),
+    legend.position = "right")
+ 
+
+# Cargamos datos y definimos distritos donde esta PECSA ----------------------------
+data_total <- readRDS(file = here::here("data", "processed", "data-final-regresiones.rds"))
+
+distritos_con_pecsa <- data_total %>% 
+  distinct(distrito, tipo_bandera) %>% 
+  filter(tipo_bandera == "PROPIA PECSA") %>% 
+  pull(distrito)
+
+# Generamos el gráfico original ----------- 
+#'Considerando todas las estaciones en distritos donde PECSA está
+
+f_labels <- data.frame(producto = c("G90", "DIESEL"), label = c("", "Venta de Pecsa"))
+
+(p2 <- data_total %>% 
+    filter(fecha <= dmy("2-10-2018"), 
+           fecha >= dmy("1-07-2017"),
+           distrito %in% distritos_con_pecsa) %>% 
+    mutate(producto = fct_relevel(producto,c("G90","DIESEL"))) %>% 
+    group_by(fecha, producto, tipo_bandera) %>% 
+    summarise(precio_promedio = mean(precio_de_venta)) %>% 
+    filter(tipo_bandera %in% c("PROPIA PRIMAX", "PROPIA PECSA")) %>%
+    mutate(tipo_bandera = fct_relevel(tipo_bandera, "PROPIA PRIMAX")) %>% 
+    ggplot() +
+    geom_line(aes(x = fecha, y = precio_promedio, linetype = tipo_bandera),
+              size = 1) +
+    geom_line(data = data_total %>%
+                filter(fecha <= dmy("2-10-2018"),
+                       fecha >= dmy("1-07-2017"),
+                       distrito %in% distritos_con_pecsa) %>%
+                mutate(producto = fct_relevel(producto,c("G90","DIESEL"))) %>% 
+                group_by(fecha, producto, tipo) %>%
+                summarise(precio_promedio = mean(precio_de_venta)) ,
+              aes(x = fecha, y = precio_promedio, color = tipo),
+              size = 1.2) +
+    facet_grid(factor(producto, levels = c("G90","DIESEL")) ~ .) +
+    geom_vline(xintercept = dmy("01-02-2018"),
+               linetype = "dashed",
+               size = 1.2, color = "grey55") +
+    geom_text(x = dmy("01-02-2018"), y = -Inf, vjust = -0.9, 
+              hjust = -0.1, aes(label = label), data = f_labels,
+              color = "grey55", fontface = "bold") +
+    labs(x = "Fecha", y = "Precio promedio", 
+         color = "Tipo de estación",
+         linetype = "Venta Pecsa") +
+    scale_color_brewer(palette = "Set1") + 
+    tema_grafica
+      
+    )
+)
+
+ggsave("precios-tipo-grifo.png", 
+       path = here::here("plots"), device = "png", unit = "cm",
+       height = 15,
+       width = 15,
+       dpi = 300)
+
+# Generamos una gráfica sin considerar grifos de Primax, y todos los distritos----------
+
+(p3 <- data_total %>% 
+    filter(fecha <= dmy("2-10-2018"), 
+           fecha >= dmy("1-07-2017"),
+           tipo_bandera != "PROPIA PRIMAX") %>% 
+    mutate(producto = fct_relevel(producto,c("G90","DIESEL")),
+           pecsa_no = if_else(tipo_bandera == "PROPIA PECSA", "PECSA", "RESTO")) %>% 
+    group_by(fecha, producto, pecsa_no) %>% 
+    summarise(precio_promedio = mean(precio_de_venta)) %>% 
+    ggplot() +
+    geom_line(aes(x = fecha, y = precio_promedio, linetype = pecsa_no),
+              size = 1) +
+    ylim(10,13) + 
+    facet_grid(factor(producto, levels = c("G90","DIESEL")) ~ .) +
+    geom_vline(xintercept = dmy("01-02-2018"),
+               linetype = "dashed",
+               size = 1.2, color = "grey55") +
+    geom_text(x = dmy("01-02-2018"), y = -Inf, vjust = -0.9, 
+              hjust = -0.1, aes(label = label), data = f_labels,
+              color = "grey55", fontface = "bold") +
+    labs(x = "Fecha", y = "Precio promedio", 
+         linetype = "Venta Pecsa") +
+    scale_color_brewer(palette = "Set1") + 
+    tema_grafica
+  
+)
+)
+
+
+
+# Gráfica sin grifos de Primax Propios, todos los distritos-----------
+#' y sin considerar vecinos a estaciones PECSA PROPIOS
+#' 
+#' 
+
+lista_vecinos <- data_total %>% 
+  filter(vecino_pecsa_thiessen_did == 1) %>%
+  distinct(codigo_de_osinergmin) %>% 
+  pull()
+(p4 <- data_total %>% 
+    filter(fecha <= dmy("2-10-2018"), 
+           fecha >= dmy("1-07-2017"),
+           tipo_bandera != "PROPIA PRIMAX",
+           codigo_de_osinergmin %ni% lista_vecinos) %>% 
+    mutate(producto = fct_relevel(producto,c("G90","DIESEL")),
+           pecsa_no = if_else(tipo_bandera == "PROPIA PECSA", "PECSA", "RESTO")) %>% 
+    group_by(fecha, producto, pecsa_no) %>% 
+    summarise(precio_promedio = mean(precio_de_venta)) %>% 
+    ggplot() +
+    geom_line(aes(x = fecha, y = precio_promedio, linetype = pecsa_no),
+              size = 1) +
+    ylim(10,13) + 
+    facet_grid(factor(producto, levels = c("G90","DIESEL")) ~ .) +
+    geom_vline(xintercept = dmy("01-02-2018"),
+               linetype = "dashed",
+               size = 1.2, color = "grey55") +
+    geom_text(x = dmy("01-02-2018"), y = -Inf, vjust = -0.9, 
+              hjust = -0.1, aes(label = label), data = f_labels,
+              color = "grey55", fontface = "bold") +
+    labs(x = "Fecha", y = "Precio promedio", 
+         linetype = "Venta Pecsa") +
+    scale_color_brewer(palette = "Set1") + 
+    tema_grafica
+  
+)
+)
+
+
+# Comparación distritos donde está PECSA con distritos donde no lo está ----------
+#' 
+
+(p5 <- data_total %>% 
+    filter(fecha <= dmy("2-10-2018"), 
+           fecha >= dmy("1-07-2017"),
+           tipo_bandera != "PROPIA PRIMAX",
+           tipo_bandera == "PROPIA PECSA" | distrito %ni% distritos_con_pecsa) %>% 
+    mutate(producto = fct_relevel(producto,c("G90","DIESEL")),
+           pecsa_no = if_else(tipo_bandera == "PROPIA PECSA", "PECSA", "RESTO")) %>% 
+    group_by(fecha, producto, pecsa_no) %>% 
+    summarise(precio_promedio = mean(precio_de_venta)) %>% 
+    ggplot() +
+    geom_line(aes(x = fecha, y = precio_promedio, linetype = pecsa_no),
+              size = 1) +
+    ylim(10,13) + 
+    facet_grid(factor(producto, levels = c("G90","DIESEL")) ~ .) +
+    geom_vline(xintercept = dmy("01-02-2018"),
+               linetype = "dashed",
+               size = 1.2, color = "grey55") +
+    geom_text(x = dmy("01-02-2018"), y = -Inf, vjust = -0.9, 
+              hjust = -0.1, aes(label = label), data = f_labels,
+              color = "grey55", fontface = "bold") +
+    labs(x = "Fecha", y = "Precio promedio", 
+         linetype = "Venta Pecsa") +
+    scale_color_brewer(palette = "Set1") + 
+    tema_grafica
+  
+)
+)
